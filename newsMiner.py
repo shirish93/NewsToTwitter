a2c7a02a71e1115d3759c663d9b6ea3b07641d67
Shirish Pokharel <shirishkpokharel@gmail.com>
2015-01-14 00:47:48 -0500

    Temporarily removed Kantipur, added republica support

--- newsMiner.py
+++ newsMiner.py
@@ -1,6 +1,36 @@
 import time
 from bs4 import BeautifulSoup
 import requests
+republica_base='http://www.myrepublica.com/portal/index.php?action=news_details&news_id='
+
+def getRepublicaLinks():
+    page = requests.get('http://myrepublica.com/portal/')
+    soup = BeautifulSoup(page.text)
+    wantedLinks = [each.get('href') for each in links
+                   if each.get('href') and
+                   'news_details' in each.get('href')]
+    ids = [int(each.split('id=')[1]) for each in wantedLinks]
+    maxID = max(ids)
+    return [republica_base+str(each) for each in ids if maxID-each<110]
+
+
+def getRepublicaArticles(linkList):
+    toReturn=[]
+    for link in linkList:
+        page = requests.get(link)
+        soup = BeautifulSoup(page.text)
+        header = soup.find_all('span', {'class': 'bodynewsheading'})[0].text
+        article = soup.find_all('div', {'class': 'mainbody'})[0].text
+        parts = article.split('-->')
+        meat = parts[1].split('Main Page')[0].strip()
+        author = meat[0:meat.index('\n')]
+        toReturn.append((header, author, meat))
+    return toReturn
+
+'''
+Cannot figure out Kantipur unicode issues:
+content won't print, even after the decoding of
+text. Will fix the issue at leisure later. -Shirish
 
 def getKantipurLinks():
 	lastDate = time.time()
@@ -12,7 +42,7 @@ def getKantipurLinks():
 	links = a.find_all('a')
 	wanted= [each.get('href') for each in links if 'full-story' in each.get('href')]
 	return wanted
-
+'''
 def getEkantipurLinks():
 	lastDate = time.time()
 	fmt='%Y/%m/%d'
